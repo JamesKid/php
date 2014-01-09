a34373e68ebfad435fff5fd431490f96ccf85d1c
JamesKid <406358964@qq.com>
2014-01-09 17:14:24 +0800

    add php execute shell

---
+++ function/execute_shell/zsj.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+# Description:  this program 
+# Editor:       zhangshijie 
+# Email:		 406358964@qq.com 
+# Version:      1.0 
+# History:      2014-01-09 creat zsj
+ls -al
+mkdir zsjff
